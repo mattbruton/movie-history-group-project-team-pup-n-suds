115dad1e84c7886f4481f1a75c91a25d5cc3b84e
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-06-07 14:20:29 -0500

    condensed watch/watched into one html partial and one controller.

--- app/controllers/WatchListCtrl.js
+++ app/controllers/WatchListCtrl.js
@@ -1,17 +1,24 @@
 "use strict";
 
-app.controller("WatchListCtrl", function($scope, DataFactory, AuthFactory) {
+app.controller("WatchListCtrl", function($scope, $location, DataFactory, AuthFactory) {
+	$scope.title = "";
+ 	$scope.movies = [];
 
-  $scope.movies = [];
+	$scope.displayMovies = function() {
+		if($location.path() === "/watch-list"){
+			DataFactory.getWatchList().then(function(data) {      
+			$scope.movies = data;
+			$scope.title = "Movie Watch List";
+			// console.log($scope.movies);
+			});
+		}else{
+			DataFactory.getWatchedList().then(function(data) {      
+			$scope.movies = data;
+			$scope.title = "Previously Viewed Movies";
+			// console.log($scope.movies);
+			});
+		}
+	};
 
-  $scope.displayWatchList = function() {
-    DataFactory.getWatchList().then(function(data) {
-      $scope.movies = data;
-      console.log($scope.movies);
-    });
-  };
-
-
-  $scope.displayWatchList();
-
+	$scope.displayMovies();
 });
